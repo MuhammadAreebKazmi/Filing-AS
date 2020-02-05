df516daa5adb5737201afc9857521b3b7d602253
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2020-02-05 22:28:07 +0500

    To output the file contents

---
+++ Filing Task 1.2.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        'Task 1.2 Output the file contents
+        Dim mName As String = ""
+        Dim IDnumber As Integer = 0
+        FileOpen(1, "C:\Filing Practice\ClubMembers.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, mName)
+            Input(1, IDnumber)
+
+            Console.WriteLine("Member name is: " & mName)
+            Console.WriteLine("Member ID number is: " & IDnumber)
+
+        End While
+        Console.ReadKey()
+
+        FileClose(1)
+    End Sub
+
+End Module
